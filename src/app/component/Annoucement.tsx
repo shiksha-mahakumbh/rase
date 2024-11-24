72f3d6e9550791468034a783136418995c6d47e4
srivastavaman <amansrivastava3516@gmail.com>
2024-11-24 10:45:56 +0530

    update social media

--- src/app/component/Annoucement.tsx
+++ src/app/component/Annoucement.tsx
@@ -52,7 +52,7 @@ const Announcement = () => {
           <h2 className="mb-4 text-xl font-semibold">Call for Papers</h2>
           <button 
             className="px-4 py-2 bg-primary text-white rounded hover:bg-white hover:text-primary"
-            onClick={() => window.location.href = '/registration/Single_Registration'}>
+            onClick={() => window.location.href = '/paper'}>
             Click Here
           </button>
         </div>
