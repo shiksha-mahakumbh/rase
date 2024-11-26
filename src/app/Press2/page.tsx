10517f1220609ed9cb5e490f890538877af0dd57
Sonal-21200 <kandarisonal21200@gmail.com>
2024-11-26 10:34:52 +0530

    picture links

--- src/app/Press2/page.tsx
+++ src/app/Press2/page.tsx
@@ -11,7 +11,7 @@ const shareUrl = encodeURIComponent("https://www.rase.co.in/Press2"); // Replace
 const shareText = encodeURIComponent(
   "कुरुक्षेत्र हरियाणा में आयोजित होगा द्वितीय शिक्षा महाकुंभ 2024।"
 );
-const shareImage = "/public/2024M/press2.jpg";
+const shareImage = "/2024M/press2.jpg";
 
 // Sample data for demonstration. Replace this with your actual data source or fetch method.
 const data = {
@@ -143,7 +143,9 @@ export default function Home() {
         {/* Social Media Sharing Section */}
         <div className="mt-6 flex justify-center space-x-4">
           <a
-            href={`https://wa.me/?text=${shareText}%20${shareUrl}&picture=${encodeURIComponent(shareImage)}`}
+            href={`https://wa.me/?text=${shareText}%20${shareUrl}&picture=${encodeURIComponent(
+      `https://www.rase.co.in${shareImage}`
+    )}`}
             target="_blank"
             rel="noopener noreferrer"
             className="p-2 bg-green-500 text-white rounded hover:bg-green-600"
@@ -151,7 +153,9 @@ export default function Home() {
             WhatsApp
           </a>
           <a
-            href={`https://www.facebook.com/sharer/sharer.php?u=${shareUrl}&quote=${shareText}&picture=${encodeURIComponent(shareImage)}`}
+            href={`https://www.facebook.com/sharer/sharer.php?u=${shareUrl}&quote=${shareText}&picture=${encodeURIComponent(
+      `https://www.rase.co.in${shareImage}`
+    )}`}
             target="_blank"
             rel="noopener noreferrer"
             className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
@@ -159,7 +163,9 @@ export default function Home() {
             Facebook
           </a>
           <a
-            href={`https://twitter.com/intent/tweet?text=${shareText}&url=${shareUrl}&picture=${encodeURIComponent(shareImage)}`}
+            href={`https://twitter.com/intent/tweet?text=${shareText}&url=${shareUrl}&picture=${encodeURIComponent(
+      `https://www.rase.co.in${shareImage}`
+    )}`}
             target="_blank"
             rel="noopener noreferrer"
             className="p-2 bg-blue-400 text-white rounded hover:bg-blue-500"
@@ -167,7 +173,9 @@ export default function Home() {
            Twitter
           </a>
           <a
-            href={`mailto:?subject=Shiksha Mahakumbh 2.0&body=${shareText}%20${shareUrl}&picture=${encodeURIComponent(shareImage)}`}
+            href={`mailto:?subject=Shiksha Mahakumbh 2.0&body=${shareText}%20${shareUrl}&picture=${encodeURIComponent(
+      `https://www.rase.co.in${shareImage}`
+    )}`}
             className="p-2 bg-gray-500 text-white rounded hover:bg-gray-600"
           >
              Email
