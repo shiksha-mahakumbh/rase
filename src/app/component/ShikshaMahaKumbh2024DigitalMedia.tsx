8161bc05229c055f6f8f0332b960f8cce3a8c169
Sonal-21200 <kandarisonal21200@gmail.com>
2024-12-02 15:11:39 +0530

    digital 1

--- src/app/component/ShikshaMahaKumbh2024DigitalMedia.tsx
+++ src/app/component/ShikshaMahaKumbh2024DigitalMedia.tsx
@@ -22,35 +22,10 @@ interface MediaItem1 {
 
 const media: MediaItem[] = [
   {
-    name: "शिक्षा महाकुंभ (RASE 2023)",
-    url: "https://youtu.be/sO8r04Y7-Q4",
-    description: "Kanhaiya Mittal Message to It and Students ",
-  },
-  {
-    name: "शिक्षा महाकुंभ (RASE 2023)",
-    url: "https://www.youtube.com/watch?v=7Kog32TfBIY",
-    description: "Interview  with Shiksha Mahakumbh Founder ",
-  },
-  {
-    name: "Dainik Savera",
-    url: "https://fb.watch/l0qAUsH5YS/",
-    description: "जालंधर मे पहली बार होने जा रहा है शिक्षा का महाकुम्भ",
-  },
-  {
-    name: "Punjab Kesari",
-    url: "https://fb.watch/n2F3e83PNr/",
-    description: "जालंधर में होने वाला शिक्षा का महाकुंभ रचेगा इतिहास, जानें क्या होगा खास?",
-  },
-  {
-    name: "Haryana Bulletin News",
-    url: "https://fb.watch/n2Fj7Bpe2p/",
-    description: "शिक्षा महाकुंभ में होगा शिक्षा के व्यवसायीकरण को लेकर मंथन",
-  },
-  {
-    name: "Real Flavours",
-    url: "https://fb.watch/kNE0I653ko/?mibextid=5Ufylb",
-    description: "Mahakumbh of Education will be held on June 9, 10, 11 at Jalandhar to discuss the curriculum of the new education policy",
-  },
+    name: "Necessity To Establish A Global Platform For Education",
+    url: "https://www.businessworld.in/article/necessity-to-establish-a-global-platform-for-education-akin-to-the-olympics-and-option-in-the-form-of-shiksha-mahakumbh-abhiyan-540875",
+    description: "Necessity To Establish A Global Platform For Education Akin To The Olympics And Option In The Form Of Shiksha Mahakumbh Abhiyan ",
+  }
 ];
 
 const ShikshaMahaKumbh2023DigitalMedia: React.FC = () => {
