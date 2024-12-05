921fd269053024bfb3beb0a21b8d502674dfaca4
Sonal-21200 <kandarisonal21200@gmail.com>
2024-12-05 11:05:58 +0530

    new gif

--- src/app/component/Annoucement.tsx
+++ src/app/component/Annoucement.tsx
@@ -46,6 +46,9 @@ const Announcement = () => {
             onClick={() => window.location.href = '/registration/Single_Registration'}>
             Click Here
           </button>
+          <span className='text-red-700 text-sm'>
+            <img src="new.gif" alt="" />
+          </span>
           <h2 className="mb-4 text-l font-semibold">Tentative Schedule Shiksha Mahakumbh 2024</h2>
           <button 
   className="px-4 py-2 bg-primary text-white rounded hover:bg-white hover:text-primary"
