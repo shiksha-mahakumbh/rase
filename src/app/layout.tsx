6cd57c6197e36c007df4dd6ebf6ae6f67fd19a26
shiksha-mahakumbh <shikshamahakumbh23@gmail.com>
2025-09-26 13:58:49 +0530

    Update layout.tsx
    Pop up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,7 +91,7 @@ export default function RootLayout({
         <Modal isOpen={isModalOpen} onClose={closeModal}>
           <div className="text-white p-4 rounded-lg flex flex-col items-center justify-center text-center text-base md:text-xl font-semibold bg-primary">
             <p>
-          <strong>शिक्षा महाकुंभ 2.0 </strong>was successfully concluded at Kurukshetra University from December 16-17, 2024. <strong> To download photographs click here</strong>:
+          <strong>शिक्षा महाकुंभ अभियान 5.0 </strong>5th Edition is going to be held at NIPER Mohali from 31st October to 2nd November 2025:
             </p>
             <p>
              <a href="https://drive.google.com/drive/folders/1XnauGu1-dQ2KCpTzvIMHhUwlBF-6GDEN" className="text-white">
