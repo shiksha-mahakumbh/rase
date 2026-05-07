efcb3808f74690787e34c84cf996943c321b4f8d
shiksha-mahakumbh <shikshamahakumbh23@gmail.com>
2026-05-07 11:55:31 +0530

    Update Annoucement.tsx

--- src/app/component/Annoucement.tsx
+++ src/app/component/Annoucement.tsx
@@ -92,27 +92,26 @@ const Announcement = () => {
         {/* </div> */}
 
         {/* व्यवस्था जानकारी शिक्षा महाकुंभ 2025 */}
-        {/* <div */}
-          {/* className={`transition-all duration-500 min-h-[200px] ${ */}
-            {/* isVisible ? "opacity-100 scale-100" : "opacity-0 scale-90" */}
-          {/* } animated-shadow bg-white p-6 rounded-lg text-center shadow-md`} */}
-        {/* > */}
-          {/* <span className="text-red-700 text-sm flex justify-center mb-2"> */}
-            {/* <img src="/new.gif" alt="New" className="w-10 h-auto" /> */}
-          {/* </span> */}
-          {/* <h2 className="mb-4 text-lg font-semibold text-gray-800"> */}
-            {/* व्यवस्था जानकारी शिक्षा महाकुंभ 2025 */}
-          {/* </h2> */}
-          {/* <button */}
-            {/* className="px-5 py-2 bg-primary text-white rounded hover:bg-white hover:text-primary border border-primary transition" */}
-            {/* onClick={() => */}
-              {/* openFile("/2024M/व्यवस्था%20जानकारी%20शिक्षा%20महाकुंभ%202025 .jpg") */}
-            {/* } */}
-          {/* > */}
-        {/* /    Click Here */}
-          {/* </button> */}
-        {/* </div> */}
-      {/* </div> */}
+         <div 
+          {className={`transition-all duration-500 min-h-[200px] ${
+            isVisible ? "opacity-100 scale-100" : "opacity-0 scale-90"
+          } animated-shadow bg-white p-6 rounded-lg text-center shadow-md`}
+         > 
+        <span className="text-red-700 text-sm flex justify-center mb-2">
+           <img src="/new.gif" alt="New" className="w-10 h-auto" /> 
+        </span>
+      <h2 className="mb-4 text-lg font-semibold text-gray-800"> 
+            व्यवस्था जानकारी शिक्षा महाकुंभ 2025 
+     </h2> 
+          <button
+    className="px-6 py-2 bg-primary text-white rounded hover:bg-white hover:text-primary border border-primary transition"
+    onClick={() =>
+      (window.location.href = "/VibhagRoute/AcademicCouncil24")
+        > 
+           Click Here
+        </button>
+        </div> 
+       </div> 
 {/* Registration Section */}
 <div
   className={`transition-all duration-500 mt-10 w-full max-w-4xl ${
